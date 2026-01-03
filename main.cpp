4df8abeb85fe89ab9e8a49d0c4bc0f5f5eb09192
FathirTHX <fathirisgaming12@gmail.com>
2026-01-03 22:06:41 +0700

    update

--- main.cpp
+++ main.cpp
@@ -6,23 +6,33 @@ int main() {
     list L;
     createList(L);
 
-    infotype p1, p2, p3;
+    infotype p1, p2, p3, p4, p5;
 
     p1.namaTanaman = "Padi";
     p1.bulanPanen = "Maret";
-    p1.jumlahPanen = 500;
+    p1.jumlahPanen = 1000;
 
     p2.namaTanaman = "Jagung";
     p2.bulanPanen = "Maret";
-    p2.jumlahPanen = 350;
+    p2.jumlahPanen = 790;
 
     p3.namaTanaman = "Labu";
-    p3.bulanPanen = "April";
-    p3.jumlahPanen = 200;
+    p3.bulanPanen = "Maret";
+    p3.jumlahPanen = 550;
+
+    p4.namaTanaman = "Padi";
+    p4.bulanPanen = "Februari";
+    p4.jumlahPanen = 850;
+
+    p5.namaTanaman = "Wortel";
+    p5.bulanPanen = "Januari";
+    p5.jumlahPanen = 500;
 
     tambahData(L, p1);
     tambahData(L, p2);
     tambahData(L, p3);
+    tambahData(L, p4);
+    tambahData(L, p5);
 
     address max = panenTerbanyak(L);
     if (max != NULL) {
